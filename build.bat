7ec9ed5ff6c84213e768100beaf3b350dd062734
blacktide <blacktide082@gmail.com>
2024-06-11 03:16:56 -0400

    Initial commit

---
+++ build.bat
@@ -0,0 +1,8 @@
+@echo off
+
+SET SITE_PACKAGES_CMD=python -c "from distutils.sysconfig import get_python_lib; print(get_python_lib())"
+FOR /F "tokens=*" %%g IN ('%SITE_PACKAGES_CMD%') do (
+    SET SITE_PACKAGES=%%g
+)
+
+pyinstaller --onefile --paths "%SITE_PACKAGES%" --add-data="hook.js:." --name ff4-name-fixer .\main.py
