6e37b4e24cb87440bd419291e9e413ee2fda8962
blacktide <blacktide082@gmail.com>
2024-06-11 23:05:10 -0400

    Add icon for executable

--- build.bat
+++ build.bat
@@ -5,4 +5,4 @@ FOR /F "tokens=*" %%g IN ('%SITE_PACKAGES_CMD%') do (
     SET SITE_PACKAGES=%%g
 )
 
-pyinstaller --onefile --paths "%SITE_PACKAGES%" --name ffiv-name-patcher .\patch.py
+pyinstaller --onefile --paths "%SITE_PACKAGES%" --name ffiv-name-patcher --icon icon.ico .\patch.py
